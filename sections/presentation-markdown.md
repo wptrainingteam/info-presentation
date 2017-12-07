e0498ed381f32fd55ffee916df05b12462465f0a
PBrocks <paul@barthmaier.rocks>
2017-12-07 16:24:58 -0500

    Initial commit

---
+++ sections/presentation-markdown.md
@@ -0,0 +1,33 @@
+<section>
+
+## Wordpress Training Team
+
+### How to Contribute
+
+<small>Created by [Sidetrack Studio](//hakim.se) / [wptrainingteam](//github.com/wptrainingteam)</small>  
+<small>Found at</small>
+
+</section>
+
+<section>
+
+## Just 3 slides
+
+This file is where you build your presentation. Next file has demo slides. Speaker notes in an aside with the class 'notes', but they won't show unless you ask Reveal to do so by hitting the S key.  
+<small>Found at</small>
+
+<aside class="notes">OhEmmGeee, are you looking for Speaker Notes?</aside>
+
+</section>
+
+<section>
+
+## Last Slide of Presentation
+
+*   Right-to-left support
+*   [Extensive JavaScript API](https://github.com/hakimel/reveal.js#api)
+*   [Auto-progression](https://github.com/hakimel/reveal.js#auto-sliding)
+*   [Parallax backgrounds](https://github.com/hakimel/reveal.js#parallax-background)
+*   [Custom keyboard bindings](https://github.com/hakimel/reveal.js#keyboard-bindings)
+
+<small>Found at</small></section>
---
+++ sections/presentation-sections.php
@@ -0,0 +1,54 @@
+			<section>
+				<h2>How to Contribute to</h2>
+				<h3>WP Training Team</h3>
+				<p>
+					<small>Created by <a href="//github.com/wptrainingteam">wptrainingteam</a></small><br>
+					<small>Found at <?php echo __FILE__; ?></small>
+				</p>
+			</section>
+
+		<section>
+			<section data-background="#dddddd">
+				<h2>Slide Backgrounds</h2>
+				<p>
+					Set <code>data-background="#dddddd"</code> on a slide to change the background color. All CSS color formats are supported.
+				</p>
+				<a href="#" class="navigate-down">
+					<img width="178" height="238" data-src="//s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Down arrow">
+				</a>
+			</section>
+			<section data-background="//s3.amazonaws.com/hakim-static/reveal-js/image-placeholder.png">
+				<h2>Image Backgrounds</h2>
+				<pre><code class="hljs">&lt;section data-background="image.png"&gt;</code></pre>
+			</section>
+			<section data-background="//s3.amazonaws.com/hakim-static/reveal-js/image-placeholder.png" data-background-repeat="repeat" data-background-size="100px">
+				<h2>Tiled Backgrounds</h2>
+				<pre><code class="hljs" style="word-wrap: break-word;">&lt;section data-background="image.png" data-background-repeat="repeat" data-background-size="100px"&gt;</code></pre>
+			</section>
+			<section data-background-color="#000000">
+				<div style="background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px;">
+					<h2>Video Backgrounds</h2>
+					<pre><code class="hljs" style="word-wrap: break-word;">&lt;section data-background-video="video.mp4,video.webm"&gt;</code></pre>
+				</div>
+			</section>
+			<section data-background-video="videos/menu-center.mp4" data-background-color="#000000">
+				<div style="background-color: rgba(0, 0, 0, 0.5); color: #fff; padding: 9px;">
+					<h4>Video Backgrounds</h4>
+				</div>
+			</section>
+			<section data-background="//i.giphy.com/90F8aUepslB84.gif">
+				<h2>... and GIFs!</h2>
+			</section>
+		</section>
+
+		<section>
+			<h2>Last Slide of Presentation</h2>
+			<ul>
+				<li>Right-to-left support</li>
+				<li><a href="//github.com/hakimel/reveal.js#api">Extensive JavaScript API</a></li>
+				<li><a href="//github.com/hakimel/reveal.js#auto-sliding">Auto-progression</a></li>
+				<li><a href="//github.com/hakimel/reveal.js#parallax-background">Parallax backgrounds</a></li>
+				<li><a href="//github.com/hakimel/reveal.js#keyboard-bindings">Custom keyboard bindings</a></li>
+			</ul><br>
+					<small>Found at <?php echo __FILE__; ?></small>
+		</section>
